973ed4d4605ac66d6fbc2b4429b77f3a8c1dc463
Liliya <liliyalik@gmail.com>
2023-09-28 16:24:26 +0300

    kontrol

---
+++ Kontr/Program.cs
@@ -0,0 +1,36 @@
+﻿string[] OriginalArray = SetArrayStr();
+Console.Clear();
+GetStrArrayMaxChar(OriginalArray);
+
+static string[] SetArrayStr(string Mesadge = "Enter the elements of the initial array separated by commas: ", char SplitChar = ',')
+{
+    Console.WriteLine(Mesadge);
+    string input = Console.ReadLine();
+    string[] originalArray = input.Split(SplitChar);
+    return originalArray;
+}
+
+static string[] GetStrArrayMaxChar(string[] originalArray)
+{
+    string[] newArray = new string[originalArray.Length];
+    int j = 0;
+    for (int i = 0; i < originalArray.Length; i++)
+    {
+        if (originalArray[i].Length <= 3)
+        {
+            newArray[j] = originalArray[i];
+            j++;
+        }
+    }
+
+    Console.WriteLine("Initial array: [" + string.Join(", ", originalArray) + "]");
+    Console.Write("New array: [");
+    for (int i = 0; i < j; i++)
+    {
+        Console.Write(newArray[i]);
+        if (i < j - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+
+    return newArray;
+}
